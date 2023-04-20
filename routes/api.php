03191e6343babfe2629eb45608d2ad7b91fad86b
Arno Princeston Pan <apan11@my.bcit.ca>
2023-04-19 17:00:03 -0700

    added error checkers and UPDATE, DELETE route

--- routes/api.php
+++ routes/api.php
@@ -35,11 +35,67 @@ Route::get("students/{student}", function(Student $student){
 //     return $student;
 // });
 
-// Post (create) student by parameters
+// Post (create) student by parameters, using create()
 Route::post('students', function(){
-    return Student::create([
-        'FirstName' => request('FirstName'),
-        'LastName' => request('LastName'),
-        'School' => request('School')
+    // validate input before creation
+    $data = request()->validate([
+        'FirstName'=>'required|string|max:255',
+        'LastName'=>'required|string|max:255',
+        'School'=>'required|string|max:255'
     ]);
+
+    try {
+        $isSuccess = Student::create($data);
+    } catch (\Exception $e) {
+        // catch an error
+        return response()->json([
+            "status" => "error",
+            "message" => "Error creating student"
+        ], 500);
+    }
+    return Student::create($data);
+});
+
+// Post (create) student by parameters, using save()
+// Route::post('students', function(){
+//     // validate input before creation
+//     $data = request()->validate([
+//         'FirstName'=>'required|string|max:255',
+//         'LastName'=>'required|string|max:255',
+//         'School'=>'required|string|max:255'
+//     ]);
+//     $student = new Student($data);
+//     $student->save();
+//     return $student;
+// });
+
+// You can add response messages with...
+// return response()->json([]);
+
+// PUT (update) student by id, implicit model binding
+Route::update("student", function(Student $student){
+    request()->validate([
+        'FirstName' => 'required|string|max:255',
+        'LastName' => 'required|string|max:255',
+        'School' => 'required|string|max:255'
+    ]);
+
+
+});
+
+// DEL (delete) student by id, implicit model binding
+Route::delete("students/{student}", function(Student $student){
+    $deleted = $student;
+    $isSuccess = $student->delete();
+    if($isSuccess){
+        return response()->json([
+            'message' => 'Deleted Successfully.',
+            'data' => $deleted,
+        ]);
+    } else {
+        return response()->json([
+            'message' => 'Deleted Successfully.',
+            'data' => $deleted,
+        ]);
+    }
 });
