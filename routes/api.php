774c90b9ce0b73f37ea119c0213a4a3e27514d2a
Arno Princeston Pan <apan11@my.bcit.ca>
2023-04-19 17:56:08 -0700

    added all other routes. Tested with ThunderClient.

--- routes/api.php
+++ routes/api.php
@@ -3,6 +3,8 @@
 use App\Models\Student;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+// use this to catch validation for request()->validate([])
+use Illuminate\Validation\ValidationException;
 
 /*
 |--------------------------------------------------------------------------
@@ -37,15 +39,30 @@ Route::get("students/{student}", function(Student $student){
 
 // Post (create) student by parameters, using create()
 Route::post('students', function(){
-    // validate input before creation
-    $data = request()->validate([
-        'FirstName'=>'required|string|max:255',
-        'LastName'=>'required|string|max:255',
-        'School'=>'required|string|max:255'
-    ]);
-
     try {
-        $isSuccess = Student::create($data);
+         // validate input before creation
+        $data = request()->validate([
+            'FirstName'=>'required|string|max:255',
+            'LastName'=>'required|string|max:255',
+            'School'=>'required|string|max:255'
+        ]);
+
+        // create new student
+        $student = Student::create($data);
+
+        return response()->json([
+            'messsage' => 'Success, Student Created.',
+            'data' => $student
+        ], 201);
+
+    } catch (ValidationException $e) {
+        // catch validation error
+        return response()->json([
+            'status' => 'error',
+            'message' => 'Validation Error.',
+            'errors' => $e->errors()
+        ], 422);
+
     } catch (\Exception $e) {
         // catch an error
         return response()->json([
@@ -53,7 +70,6 @@ Route::post('students', function(){
             "message" => "Error creating student"
         ], 500);
     }
-    return Student::create($data);
 });
 
 // Post (create) student by parameters, using save()
@@ -73,14 +89,31 @@ Route::post('students', function(){
 // return response()->json([]);
 
 // PUT (update) student by id, implicit model binding
-Route::update("student", function(Student $student){
-    request()->validate([
-        'FirstName' => 'required|string|max:255',
-        'LastName' => 'required|string|max:255',
-        'School' => 'required|string|max:255'
-    ]);
+Route::put("students/{student}", function(Student $student){
+    try{
+        $data = request()->validate([
+            'FirstName' => 'required|string|max:255',
+            'LastName' => 'required|string|max:255',
+            'School' => 'required|string|max:255'
+        ]);
 
+        $student->update($data);
 
+        return response()->json([
+            'message' => 'Updated Successfully.',
+            'data' => $student
+        ], 200);
+    } catch (ValidationException $e) {
+        return response()->json([
+            'status' => 'error',
+            'message' => 'Validation Error.'
+        ], 422);
+    } catch (\Exception $e) {
+        return response()->json([
+            'status' => 'error',
+            'message' => 'Error updating student.'
+        ], 500);
+    }
 });
 
 // DEL (delete) student by id, implicit model binding
